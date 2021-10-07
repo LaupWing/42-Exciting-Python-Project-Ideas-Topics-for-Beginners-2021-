d9e6ee3ccaa8e1393b96bfa4b8bb686e8124e99f
LaupWing <loc_strict@hotmail.com>
2021-10-07 21:17:40 +0200

    update

--- 1.mad_libs.py
+++ 1.mad_libs.py
@@ -3,19 +3,19 @@ from random import randrange
 correct = False
 questions =  [
    {
-      "question":"Basketball is the best ... in the world",
+      "question":"Basketball is the best {answer} in the world",
       "answer":"sport"
    },
    {
-      "question": "Love is what makes the ... go round.",
+      "question": "Love is what makes the {answer} go round.",
       "answer": "world"
    },
    {
-      "question":"My gym locker stinks because I'm always leaving my dirty ... in there!!",
+      "question":"My gym locker stinks because I'm always leaving my dirty {answer} in there!!",
       "answer":"socks",
    },
    {
-      "question":"Once that ... music comes on, it's time to shut down your acceptance speech!",
+      "question": "Once that {answer} music comes on, it's time to shut down your acceptance speech!",
       "answer": "radio"
    }
 ]
@@ -31,6 +31,7 @@ while not correct:
    answer = input('My answer is: ')
    if answer == question_dict["answer"]:
       correct = True
-      print("correct!")
+      print(format(question_dict["question"]))
    else:
       print("incorrect")
+      print(question_dict["question"].format(answer))
