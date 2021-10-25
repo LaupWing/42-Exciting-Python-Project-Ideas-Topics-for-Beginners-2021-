40734ae2b937dfbf87e467aeec7ab4702124dda7
LaupWing <loc_strict@hotmail.com>
2021-10-25 14:22:29 +0200

    finished

--- 6.contact_book.py
+++ 6.contact_book.py
@@ -5,8 +5,25 @@ connection = sqlite3.connect("contact.db")
 cursor  = connection.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS
-contacts(phonenumber INTEGER, name TEXT)""")
-cursor.execute("INSERT INTO contacts VALUES ('0654754116','Loc Nguyen')")
+contacts(phonenumber TEXT, name TEXT)""")
 
-connection.commit()
-connection.close()
+
+while True:
+   try:
+      choice = int(input('Press 1 to show contacts and 2 to add: '))
+      if choice == 1:
+         name = input('Name: ')
+         phonenumber = input('Phonenumber: ')
+         cursor.execute(f"INSERT INTO contacts VALUES ('{phonenumber}','{name}')")
+         connection.commit()
+         connection.close()
+         break
+      elif choice == 2:
+         for row in cursor.execute('SELECT * FROM contacts ORDER BY name'):
+            print(row)
+         connection.close()
+         break
+      else:
+         raise ValueError('Invalid input')
+   except:
+      print('Not a valid input')
